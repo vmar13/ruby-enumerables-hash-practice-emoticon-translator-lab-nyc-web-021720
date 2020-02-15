93f22892afa9b22b9cc847d90783d2768e7db8d1
Vanessa Martinez <vmar76@gmail.com>
2020-02-15 17:49:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,8 +7,19 @@ def load_library(file_path)
   hash["get_meaning"] = {}
   hash["get_emoticon"] = {}
   binding.pry 
-  # emoticons.each do |
+  emoticons.each do |
 end
+
+
+# emoticons.each do |japanese_emoticon,meaning|
+#         if result['get_meaning'] then
+#           expect(result['get_meaning'][japanese_emoticon]).to eq(meaning)
+#         else
+#           expect(result[:get_meaning][japanese_emoticon]).to eq(meaning)
+#         end
+#       end
+#     end
+
 
 
 def get_japanese_emoticon
