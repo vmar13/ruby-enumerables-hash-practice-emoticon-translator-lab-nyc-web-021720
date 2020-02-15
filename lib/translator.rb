fbd59a1caae6237b82adfece42cff174ae465e19
Vanessa Martinez <vmar76@gmail.com>
2020-02-15 18:37:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,9 +6,9 @@ def load_library(file_path)
   hash = {}
   hash["get_meaning"] = {}
   hash["get_emoticon"] = {}
-  # binding.pry 
   emoticons.each do |meaning, emoticon_array|
     hash["get_meaning"][emoticon_array[1]] = meaning
+    binding.pry 
     hash["get_emoticon"][emoticon_array[0]] = emoticon_array[1]
   end 
   hash 
@@ -16,9 +16,10 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   load_library(file_path)
+  hash["get_emoticon"][emoticon_array]
 end
 
-#returns the Japanese equivalent of an English grinning (FAILED - 3)
+  #returns the Japanese equivalent of an English grinning (FAILED - 3)
   # returns the Japanese equivalent of an English happy (FAILED - 4)
   # returns the Japanese equivalent of an English sad (FAILED - 5)
   # returns an apology message if the argument is not a known emoticon
