82e4fd189f38a2fbf412a505d9fd9737008f2cf3
Vanessa Martinez <vmar76@gmail.com>
2020-02-15 18:33:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,10 @@ def get_japanese_emoticon(file_path, emoticon)
   load_library(file_path)
 end
 
-#
+#returns the Japanese equivalent of an English grinning (FAILED - 3)
+  # returns the Japanese equivalent of an English happy (FAILED - 4)
+  # returns the Japanese equivalent of an English sad (FAILED - 5)
+  # returns an apology message if the argument is not a known emoticon
 
 # def get_english_meaning
 #   # code goes here
