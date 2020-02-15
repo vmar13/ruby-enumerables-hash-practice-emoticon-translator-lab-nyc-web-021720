6c0e2c2f0cbad61a390b4a8c966e1eb43ca6c796
Vanessa Martinez <vmar76@gmail.com>
2020-02-15 18:29:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,21 +15,12 @@ def load_library(file_path)
 end
 
 
-# emoticons.each do |japanese_emoticon,meaning|
-#         if result['get_meaning'] then
-#           expect(result['get_meaning'][japanese_emoticon]).to eq(meaning)
-#         else
-#           expect(result[:get_meaning][japanese_emoticon]).to eq(meaning)
-#         end
-#       end
-#     end
 
 
+# def get_japanese_emoticon
+#   # code goes here
+# end
 
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
+# def get_english_meaning
+#   # code goes here
+# end
